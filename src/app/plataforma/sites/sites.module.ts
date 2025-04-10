9aec721f6c195a7c555d7d20a8d92ae622d09b82
JohnCastañeda <jhoncastaneda128@gmail.com>
2025-04-10 17:01:17 -0500

    10-04-2025

--- src/app/plataforma/sites/sites.module.ts
+++ src/app/plataforma/sites/sites.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SitesRoutingModule } from './sites-routing.module';
 import { SharedModule } from '../../theme/shared/shared.module';
+import { BreadcrumbComponent } from 'src/app/theme/shared/components/breadcrumb/breadcrumb.component';
 
 @NgModule({
   imports: [
     CommonModule, SitesRoutingModule, SharedModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [BreadcrumbComponent]
 })
 export class SitesModule { }
